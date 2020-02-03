84439652f1f01456aeee21fceddf9b3b2c62b482
Edwin Dominguez <edwindominguez@hotmail.com>
2020-02-02 23:16:50 -0500

    add search bar component, use youtube api to fetch data using axios

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
+export default class App extends Component {
+  onTermSubmit = term => {
+    youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className='ui container' style={{ marginTop: '10px' }}>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+      </div>
+    );
+  }
+}
