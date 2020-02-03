76379b23ca2a5d317fe8ded3c1c1f42f8ca25cb0
Edwin Dominguez <edwindominguez@hotmail.com>
2020-02-02 23:44:26 -0500

    add video item and list components, render thumbnails in video item

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 
 import SearchBar from './SearchBar';
+import VideList from './VideoList';
 import youtube from '../apis/youtube';
 
 export default class App extends Component {
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideList videos={this.state.videos} />
       </div>
     );
   }
